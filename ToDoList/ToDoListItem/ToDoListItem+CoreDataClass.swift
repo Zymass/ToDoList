7f7d915cfdc48cac9bf45415b221eca24d8456a4
Zymass <70097857+Zymass@users.noreply.github.com>
2021-10-27 18:00:05 +0300

    ToDoList with CoreData

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Филяев Илья on 27.10.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
